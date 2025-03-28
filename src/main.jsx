5837ec11ca86b1f9d19bafcce3061139ec48a5ce
Serhii <serhii1992ser@gmail.com>
2025-03-28 18:10:42 +0100

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { store, persistor } from "./redux/store";
 import App from "./App";
 import "./index.css";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
